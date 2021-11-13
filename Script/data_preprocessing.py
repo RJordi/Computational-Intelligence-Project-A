8ee37755856f99427c055cc0b13e180d4075eb4d
victorpara1407 <victor.parra@rwth-aachen.de>
2021-11-13 10:45:34 +0100

    Update data_preprocessing.py

--- Script/data_preprocessing.py
+++ Script/data_preprocessing.py
@@ -5,14 +5,19 @@ import os
 from sklearn.decomposition import PCA
 
 
-path = os.getcwd()
 
+## Save this file within the downloaded folder "Smartphone1"
+
+
+path = os.getcwd()
 subject = np.arange(150, 174, 1).tolist()
 rep = ["01","02"]
 
 for i in subject:
     for j in rep:
         try:
+            ## Warning: This loop overwrites the data frame. 
+
             df = pd.read_csv(path + "\\subject"+str(i)+"_downstairs"+j+"\Accelerometer.csv")
             df = df.drop( columns = df.columns[0])
         except: 
