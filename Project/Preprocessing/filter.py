874b0feb42a7816c3d5f6202f0b435f7d691840d
Jordi Ribas <jordi.ribas@rwth-aachen.de>
2021-11-18 12:28:55 +0100

    Updated some names and files

--- Project/Preprocessing/filter.py
+++
@@ -1,6 +0,0 @@
-from scipy.signal import savgol_filter
-
-def filter(experiment)
-    experiment.x = savgol_filter(experiment.x, 70, 3)
-    experiment.y = savgol_filter(experiment.y, 70, 3)
-    experiment.z = savgol_filter(experiment.z, 70, 3)
