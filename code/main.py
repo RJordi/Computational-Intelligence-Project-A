68d11843fa15d68815f05c86b0ca0a55a50a0498
Jordi Ribas <jordi.ribas@rwth-aachen.de>
2021-11-30 19:02:15 +0100

    Implemented k-folding subject-wise

--- code/main.py
+++ code/main.py
@@ -8,9 +8,9 @@ import main_ann
 
 print('-------- PROGRAM STARTED --------')
 
-samples_tensor, labels_array = main_preprocess.main_preprocess()
+samples_tensor, labels_array, subjects_array = main_preprocess.main_preprocess()
 
 #print('Shape of samples tensor:', np.shape(samples_tensor))
 #print('Shape of labels array:', np.shape(labels_array))
 
-main_ann.main_ann(samples_tensor, labels_array)
+main_ann.main_ann(samples_tensor, labels_array, subjects_array)
--- code/preprocessing/arrange_data_for_ann.py
+++ code/preprocessing/arrange_data_for_ann.py
@@ -3,7 +3,8 @@ import numpy as np
 def arrange_data (resampled_list):
 
 	samples_tensor = []
-	labels_array = [] # change to correct value
+	labels_array = []
+	subjects_array = []
 
 	for i in range(len(resampled_list)):
 		sample = resampled_list[i]
@@ -17,11 +18,17 @@ def arrange_data (resampled_list):
 
 		# append label to labels_array FOR EACH STEP AND EACH SAMPLE
 		labels_array += [sample.label]*sample.number_of_steps
+
+		# append subject to subjects_array FOR EACH STEP AND EACH SAMPLE
+		subjects_array += [sample.title[0:3]]*sample.number_of_steps
 	
 	samples_tensor = np.vstack(samples_tensor)
 	labels_array = np.asarray(labels_array)
-	#print('Shape of samples tensor:', np.shape(samples_tensor))
-	#print('Shape of labels array:', np.shape(labels_array))
+	subjects_array = np.asarray(subjects_array)
+	print('Shape of samples tensor:', np.shape(samples_tensor))
+	print('Shape of labels array:', np.shape(labels_array))
+	print('Shape of subjects array:', np.shape(subjects_array))
 	#print(labels_array)
+	#print(subjects_array)
 
-	return samples_tensor, labels_array
+	return samples_tensor, labels_array, subjects_array
